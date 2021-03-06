732c3126c7d7e89570f6db59ae93364924a18fa4
stledesma <st19.killer@gmail.com>
2021-03-06 12:27:15 -0500

    componente alert-delete

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,10 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import Vue from 'vue';
+
 
 require('./bootstrap');
 
@@ -19,6 +23,7 @@ window.Vue = require('vue');
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
+Vue.use(VueSweetalert2);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('eliminar-receta', require('./components/eliminarReceta.vue').default);
 Vue.config.ignoredElements = ['trix-editor', 'trix-toolbar'];
--- resources/js/components/eliminarReceta.vue
+++ resources/js/components/eliminarReceta.vue
@@ -10,7 +10,50 @@ export default {
     },
     methods:{
         eliminarReceta(){
-            console.log('hola', this.idReceta)
+            this.$swal.fire({
+            title: 'Esta seguro de eliminar esta receta?',
+            width: 600,
+            padding: '3em',
+            background: '#fff url(/images/trees.png)',
+            backdrop: `
+                rgba(0,0,123,0.4)
+                url("/images/gif-anime.gif")
+                left top
+                no-repeat
+            `,
+            text: "Esta accion no sera reversible!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#47C11F',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, eliminar receta!',
+             cancelButtonText: 'Cancelar'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                const params={
+                    id:this.idReceta
+                }
+                axios.post(`/recetas/${this.idReceta}`,{params, _method:'delete'})
+                .then(respuesta=>{
+                    this.$swal.fire(
+                    'Receta eliminada!',
+                    'Tu receta se ha eliminado.',
+                    'success'
+                    )
+                    this.$el.parentNode.parentNode.parentNode.removeChild(this.$el.parentNode.parentElement);
+                })
+                .catch(error=>{
+                    console.log(error);
+                })
+
+            } else {
+                this.$swal.fire(
+                'Accion Cancelada',
+                'Tu receta no ha sido eliminada',
+                'error'
+                )
+            }
+            })
         }
     }
 
